2781962b48e0031e35cb9d62449ffe9858a5f44b
Josiah Maius S. Ebia <josiah_ebia@yahoo.com>
2017-10-27 14:35:45 +0800

    Added file chooser and next/prev functions

--- SpriteViewer/src/app/Controller.java
+++ SpriteViewer/src/app/Controller.java
@@ -1,14 +1,22 @@
 package app;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 
 public class Controller {
 
+    @FXML private GridPane gridPane;
 
     @FXML private TextField textHeight;
     @FXML private TextField textWidth;
@@ -22,5 +30,55 @@ public class Controller {
     @FXML private Button buttonPrev;
     @FXML private Button buttonNext;
     @FXML private Button buttonOpenFile;
+
+    private ViewerModel vm;
+
+    @FXML
+    void openFile(ActionEvent e) {
+
+        Stage stage = (Stage) gridPane.getScene().getWindow();
+
+        FileChooser.ExtensionFilter imageFilter
+                = new FileChooser.ExtensionFilter("Image Files", "*.jpg", "*.png");
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Spritesheet File");
+        fileChooser.getExtensionFilters().add(imageFilter);
+        File file = fileChooser.showOpenDialog(stage);
+
+        Image image = new Image(file.toURI().toString());
+
+        vm = new ViewerModel(image, Integer.parseInt(textHeight.getText()), Integer.parseInt(textWidth.getText()),
+                Integer.parseInt(textCol.getText()));
+
+    }
+
+    @FXML
+    void nextView (ActionEvent e) {
+        try {
+
+            if (vm.getCurrentIndex() < vm.getMaxIndex())
+                vm.setCurrentIndex(vm.getCurrentIndex() + 1);
+            else
+                vm.setCurrentIndex(vm.getMinIndex());
+
+        } catch (NullPointerException exception) {
+
+        }
+    }
+
+    @FXML
+    void prevView (ActionEvent e) {
+        try {
+
+            if (vm.getCurrentIndex() > vm.getMinIndex())
+                vm.setCurrentIndex(vm.getCurrentIndex() - 1);
+            else
+                vm.setCurrentIndex(vm.getMaxIndex());
+
+        } catch (NullPointerException exception) {
+
+        }
+    }
 
 }
---
+++ SpriteViewer/src/app/ViewerModel.java
@@ -0,0 +1,82 @@
+package app;
+
+import javafx.scene.image.Image;
+
+public class ViewerModel {
+
+    private Image spritesheet;
+    private int spriteHeight;
+    private int spriteWidth;
+    private int animColumns;
+
+    private int currentIndex;
+    private int maxIndex;
+    private int minIndex;
+
+    public ViewerModel(Image image, int h, int w, int col) {
+        spritesheet = image;
+        spriteHeight = h;
+        spriteWidth = w;
+        animColumns = col;
+
+        minIndex = 0;
+        maxIndex = col - 1;
+        currentIndex = minIndex;
+    }
+
+    public Image getSpritesheet() {
+        return spritesheet;
+    }
+
+    public void setSpritesheet(Image spritesheet) {
+        this.spritesheet = spritesheet;
+    }
+
+    public int getSpriteHeight() {
+        return spriteHeight;
+    }
+
+    public void setSpriteHeight(int spriteHeight) {
+        this.spriteHeight = spriteHeight;
+    }
+
+    public int getSpriteWidth() {
+        return spriteWidth;
+    }
+
+    public void setSpriteWidth(int spriteWidth) {
+        this.spriteWidth = spriteWidth;
+    }
+
+    public int getAnimColumns() {
+        return animColumns;
+    }
+
+    public void setAnimColumns(int animColumns) {
+        this.animColumns = animColumns;
+    }
+
+    public int getCurrentIndex() {
+        return currentIndex;
+    }
+
+    public void setCurrentIndex(int currentIndex) {
+        this.currentIndex = currentIndex;
+    }
+
+    public int getMaxIndex() {
+        return maxIndex;
+    }
+
+    public void setMaxIndex(int maxIndex) {
+        this.maxIndex = maxIndex;
+    }
+
+    public int getMinIndex() {
+        return minIndex;
+    }
+
+    public void setMinIndex(int minIndex) {
+        this.minIndex = minIndex;
+    }
+}
